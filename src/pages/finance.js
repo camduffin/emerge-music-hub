9003624a8f0888991a50a3f97dae54c8c016fcc6
cameronduffin <camduffin@gmail.com>
2021-04-17 15:41:10 -0400

    adding finance page animations

--- src/pages/finance.js
+++ src/pages/finance.js
@@ -8,7 +8,7 @@ const Finance = () => {
             <Layout>
                 <section className="wrapper">
                     <ul className="finance-video-list">
-                        <li className="video video1">
+                        <li className="video video1" data-sal="fade" data-sal-duration="1500">
                             <h2>Sunny Widerman</h2>
                             <h3>Tax Preparer and Advisor, CEO Personal Tax Advisors</h3>
                             <iframe
@@ -25,11 +25,10 @@ const Finance = () => {
                     </ul>
                 </section>
                 <section className="grant-tips-section">
-                    <div className="wrapper grant-text-container">
+                    <div className="wrapper grant-text-container" data-sal="fade" data-sal-duration="1500">
                         <h2>Grant Writing Tips</h2>
                         <h3>Margaret O'Toole</h3>
                         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat obcaecati enim voluptatem officiis blanditiis fugit earum ea omnis recusandae minus eligendi delectus sint soluta, itaque aut amet possimus repellendus! Aspernatur doloribus dolorem deleniti eaque! Quidem aut impedit vero, facilis earum a tempora velit incidunt accusantium obcaecati perspiciatis itaque veniam odit quaerat est, expedita, ullam fugiat porro sed sint eos vel. Ipsam iure quidem dolorem sequi illum a laborum. Non sequi dolor aliquam soluta quos. Quibusdam quidem nostrum voluptatibus numquam illum dolores, eius labore autem sapiente, animi molestiae nihil mollitia. Fuga soluta saepe, sed alias maxime, commodi omnis eligendi sit similique dicta minima nam ab officiis nulla consequatur, ratione reiciendis mollitia hic. Id officiis porro, qui quaerat autem sapiente totam quidem nisi, nulla libero quasi cum consequatur ex soluta animi quisquam fugiat nesciunt dolorem veritatis accusamus laborum placeat aspernatur maiores! A ratione veniam maxime officiis beatae consequatur inventore amet. Blanditiis eaque officia, quia provident itaque tempore molestiae quisquam neque alias minus. Repellendus facere quo animi quasi earum autem minus dolor soluta illo! Quam ab optio impedit corrupti ipsam odit aspernatur quos quo autem est praesentium nam consequatur at modi tenetur fugiat hic cum in natus, facere perspiciatis! Accusamus, doloribus! Ad sapiente possimus est asperiores consequuntur dolorum exercitationem obcaecati minima voluptatum earum. Minus ducimus, quos corporis eius quo natus in pariatur nostrum ut amet voluptatum perferendis aperiam consectetur. Minima odit exercitationem sapiente nemo, ut provident quis, natus tempore architecto dicta ad iure atque? Iste earum voluptate asperiores nisi, harum eaque saepe! Incidunt ipsa dolorum fugit iure, provident id sunt amet, repellendus praesentium aspernatur odit hic. Id alias reprehenderit corporis vero quae voluptatibus dignissimos quaerat deserunt aspernatur deleniti doloribus molestiae rerum vitae, ut nihil quos labore nemo sed, asperiores quasi iusto. Itaque earum sequi voluptatibus aliquid quis ipsam saepe fugiat assumenda blanditiis ullam.</p>
-
                     </div>
                 </section>
             </Layout>
